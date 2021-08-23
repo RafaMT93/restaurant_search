09abf590c7928ead7c7d002b41484bd491c2af5c
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-08-23 20:05:20 -0300

    Search Input and your style added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import logo from '../assets/logo.svg';
+import { Container, Search } from './styles';
+import styles from './Home.module.css';
 
 const Home = () => {
-  return <Container>Hello World!</Container>;
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do restaurante" className={styles.img} />
+        <TextField
+          label="Search"
+          outlined
+          //onTrailingIconSelect={() => this.setState({ value: '' })}
+          //trailingIcon={<MaterialIcon role="button" icon="delete" />}
+        >
+          <Input value={inputValue} onChange={({ target }) => setInputValue(target.value)} />
+        </TextField>
+      </Search>
+    </Container>
+  );
 };
 
 export default Home;
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background-color: ${({ theme }) => theme.colors.primary};
+export const Container = styled.aside`
+  background-color: ${({ theme }) => theme.colors.background};
+  width: 22.5rem;
+  height: 100vh;
+  overflow-y: auto;
+`;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 18px;
 `;
