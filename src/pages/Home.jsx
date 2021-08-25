2d2750e31843f5ed597c0af31ec121e35624d180
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-08-24 22:05:28 -0300

    Material icon added and your dependencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import logo from '../assets/logo.svg';
-import { Container, Search } from './styles';
-import styles from './Home.module.css';
+import { Container, Search, Logo, Wrapper, Map } from './styled-component/styles';
+import MaterialIcon from '@material/react-material-icon';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
 
   return (
-    <Container>
-      <Search>
-        <img src={logo} alt="Logo do restaurante" className={styles.img} />
-        <TextField
-          label="Search"
-          outlined
-          //onTrailingIconSelect={() => this.setState({ value: '' })}
-          //trailingIcon={<MaterialIcon role="button" icon="delete" />}
-        >
-          <Input value={inputValue} onChange={({ target }) => setInputValue(target.value)} />
-        </TextField>
-      </Search>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do restaurante" />
+          <TextField
+            label="Pesquisar Restaurantes"
+            outlined
+            //onTrailingIconSelect={() => this.setState({ value: '' })}
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={({ target }) => setInputValue(target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
   );
 };
 
--- src/pages/styled-component/styles.js
+++ src/pages/styled-component/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 export const Container = styled.aside`
   background-color: ${({ theme }) => theme.colors.background};
   width: 22.5rem;
@@ -13,4 +18,15 @@ export const Search = styled.section`
   justify-content: center;
   background-color: #ffffff;
   padding: 18px;
+`;
+
+export const Logo = styled.img`
+  width: 50%;
+  margin-bottom: 1.25rem;
+  margin-left: 25%;
+`;
+
+export const Map = styled.div`
+  background-color: red;
+  width: 100%;
 `;
